33b35eb5c2633fcf6dac4c8913fe2901b6cf70a6
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-23 15:29:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+ #array = ["kiwi", "durian", "starfruit", "mango", "dragon fruits"].join (" ")
+ if array.length == 1
+   array.join (" ")
+ elsif array.length == 2
+  array.join (" and ")
+elsif array.length == 3
+  array.insert ( " , ")
+  array.join (2)
+else
+end
 end
